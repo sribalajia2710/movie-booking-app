700bd2303bb87d7dbb87e695e65815c8340c0b9e
Sribalaji <sribalaji.anbarasu@outlook.com>
2023-02-20 01:14:53 +0530

    fixed warnings

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import logo from "../../assets/logo.png";
 import "./footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -10,20 +11,28 @@ function Footer() {
           <span>Showtime</span>
         </div>
         <div className="links">
-          <a href="#">About</a>
-          <a href="#">FAQ</a>
-          <a href="#">Contact Us</a>
-          <a href="#">Terms of Use</a>
+          <Link className="header-link" to="/movies">
+            About
+          </Link>
+          <Link className="header-link" to="/theatres">
+            FAQs
+          </Link>
+          <Link className="header-link" to="/signin">
+            Contact us
+          </Link>
+          <Link className="header-link" to="/signin">
+            Terms of Use
+          </Link>
         </div>
         <p style={{ fontWeight: "bold" }}>&copy; 2023 Sribalaji &trade;</p>
         <div className="social-icons">
-          <a href="https://www.facebook.com" target="_blank">
+          <a href="https://www.facebook.com">
             <i className="fab fa-facebook"></i>
           </a>
-          <a href="https://www.twitter.com" target="_blank">
+          <a href="https://www.twitter.com">
             <i className="fab fa-twitter"></i>
           </a>
-          <a href="https://www.instagram.com" target="_blank">
+          <a href="https://www.instagram.com">
             <i className="fab fa-instagram"></i>
           </a>
         </div>
